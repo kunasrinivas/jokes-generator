3699a198b343ba00ea0024a01714c51c5b44c662
skuna <kunasrinivas@gmail.com>
2021-03-10 21:12:34 +0100

    set up the controller part

--- src/main/java/com/example/norris/chuck/jokesgenerator/controllers/JokesGeneratorController.java
+++ src/main/java/com/example/norris/chuck/jokesgenerator/controllers/JokesGeneratorController.java
@@ -1,12 +1,22 @@
 package com.example.norris.chuck.jokesgenerator.controllers;
 
+import com.example.norris.chuck.jokesgenerator.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokesGeneratorController {
-    @Mapping("/")
-    public void generateJoke(){
-        JokesGeneratorController
+    private final JokeService jokeService;
+    @Autowired
+    public JokesGeneratorController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+        return ("index");
     }
 }
