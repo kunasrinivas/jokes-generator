3cacbdf27fbf76bd0340bd0472081350d8f12679
skuna <kunasrinivas@gmail.com>
2021-03-10 21:04:17 +0100

    initial version with service

---
+++ src/main/java/com/example/norris/chuck/jokesgenerator/controllers/JokesGeneratorController.java
@@ -0,0 +1,12 @@
+package com.example.norris.chuck.jokesgenerator.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+
+@Controller
+public class JokesGeneratorController {
+    @Mapping("/")
+    public void generateJoke(){
+        JokesGeneratorController
+    }
+}
---
+++ src/test/java/com/example/norris/chuck/jokesgenerator/JokesGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.norris.chuck.jokesgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
